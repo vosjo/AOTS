2b8a5240c7cc3679399f41a5a3a5ae4f73cc1e79
ostlight <rhainich@astro.physik.uni-potsdam.de>
2021-11-17 22:52:59 +0100

    Two bug fixes -> filter menu for system list

--- AOTS/custom_permissions.py
+++ AOTS/custom_permissions.py
@@ -8,53 +8,68 @@ from stars.models import Project
 
 class IsAllowedOnProject(permissions.BasePermission):
    """
-   Custom permission to allow users to see/edit/add/remove objects only if 
+   Custom permission to allow users to see/edit/add/remove objects only if
    they have permission to perform those actions for the project this object belongs to.
    """
-   
+
    def has_object_permission(self, request, view, obj):
-      
+
       # show the object if the user is allowed to see this project (GET, HEAD or OPTIONS)
       if request.method in permissions.SAFE_METHODS:
          if request.user.is_anonymous:
             return obj.project.is_public
          else:
             return request.user.can_read(obj.project)
-      
+
       # User can add objects in this project?
       if request.method == ['POST'] and not request.user.is_anonymous:
          return request.user.can_add(obj.project)
-      
+
       # check if the user is allowed to edit/delete this specific object
       if request.method in ['PUT', 'PATCH', 'DELETE'] and not request.user.is_anonymous:
          return request.user.can_edit(obj)
-      
-      
+
+
       return False
 
 
 def get_allowed_objects_to_view_for_user(qs, user):
-   """
-   Function that will limit the provided queryset to the objects that the provided user can see.
-   
-   This filtering is based on the project that the object belongs too. An anonymous user can 
-   see objects from all public projects. A logged in user can also see private projects that 
-   he/she has viewing rights for.
-   
-   for some reason qs1.union(qs2) can not be used here instead of using th | operator!!!
-   """
-   
-   public = qs.filter(project__is_public__exact=True)
-   
-   if user.is_anonymous:
-      return public
-   else:
-      return qs.filter(project__pk__in=user.get_read_projects().values('pk')) | public
-   
+    """
+    Function that will limit the provided queryset to the objects that
+    the provided user can see.
+
+    This filtering is based on the project that the object belongs too.
+    An anonymous user can see objects from all public projects. A logged
+    in user can also see private projects that he/she has viewing rights
+    for.
+
+    (for some reason qs1.union(qs2) can not be used here instead of
+    using th | operator!!!)
+    """
+
+    #   Check if project is public
+    public = qs.filter(project__is_public__exact=True)
+
+    #   Check if user is logged in ...
+    if user.is_anonymous:
+        #   ... return the "public" queryset if not
+        return public
+    else:
+        #   Check if user is allowed to view the project ...
+        restricted = qs.filter(
+            project__pk__in=user.get_read_projects().values('pk')
+            )
+        if len(restricted) > 0:
+            #   ... if this is the case return the specific queryset ...
+            return restricted
+        else:
+            #   ... if not, return the public queryset
+            return public
+
 
 def check_user_can_view_project(function):
    """
-   Decorator that loads the function if the user is allowed to see the project, 
+   Decorator that loads the function if the user is allowed to see the project,
    redirects to login page otherwise.
    """
    def wrapper(request, *args, **kwargs):
@@ -64,7 +79,7 @@ def check_user_can_view_project(function):
       except Exception:
          messages.error(request, "That page requires login to view")
          return redirect('login')
-      
+
       if request.user.is_anonymous and project.is_public:
          return function(request, *args, **kwargs)
       elif not request.user.is_anonymous and request.user.can_read(project):
@@ -72,5 +87,5 @@ def check_user_can_view_project(function):
       else:
          messages.error(request, "Project: {} requires login to see".format(project))
          return redirect('login')
-   
+
    return wrapper
--- stars/api/views.py
+++ stars/api/views.py
@@ -1,8 +1,8 @@
- 
+
 #from rest_framework.generics import (
    #CreateAPIView,
    #DestroyAPIView,
-   #ListAPIView, 
+   #ListAPIView,
    #UpdateAPIView,
    #RetrieveAPIView,
    #RetrieveUpdateAPIView
@@ -44,7 +44,7 @@ class ProjectViewSet(viewsets.ModelViewSet):
    """
    queryset = Project.objects.all()
    serializer_class = ProjectSerializer
-   
+
    def list(self, request):
       queryset = Project.objects.all()
       serializer = ProjectListSerializer(queryset, many=True)
@@ -57,108 +57,168 @@ class ProjectViewSet(viewsets.ModelViewSet):
 
 
 class StarFilter(filters.FilterSet):
-   
-   
-   name = filters.CharFilter(field_name="name", method='filter_name', lookup_expr='icontains')
-   
-   coordinates = filters.CharFilter(field_name="ra", method='filter_coordinates', lookup_expr='icontains')
-   
-   ra = filters.RangeFilter(field_name="ra",)
-   
-   dec = filters.RangeFilter(field_name="dec",)
-   
-   mag_min = filters.NumberFilter(field_name="Gmag", method='filter_magnitude_gt', lookup_expr='gte')
-   mag_max = filters.NumberFilter(field_name="Gmag", method='filter_magnitude_lt', lookup_expr='lte')
-   
-   classification = filters.CharFilter(field_name="classification", lookup_expr='icontains')
-   
-   classification_type = filters.MultipleChoiceFilter(field_name="classification_type", choices=Star.CLASSIFICATION_TYPE_CHOICES)
-   
-   status = filters.MultipleChoiceFilter(field_name="observing_status", choices=Star.OBSERVING_STATUS_CHOICES)
-   
-   tags = filters.ModelMultipleChoiceFilter(queryset=Tag.objects.all())
-   
-   #name = filters.CharFilter(field_name='name', method='filter_identifier', lookup_expr='icontains')
-   
-   nphot_min = filters.NumberFilter(field_name="photometry", method='filter_nphot_gt', lookup_expr='gte')
-   nphot_max = filters.NumberFilter(field_name="photometry", method='filter_nphot_lt', lookup_expr='lte')
-   
-   nspec_min = filters.NumberFilter(field_name="spectrum", method='filter_nspec_gt', lookup_expr='gte')
-   nspec_max = filters.NumberFilter(field_name="spectrum", method='filter_nspec_lt', lookup_expr='lte')
-   
-   nlc_min = filters.NumberFilter(field_name="lightcurve", method='filter_nlc_gt', lookup_expr='gte')
-   nlc_max = filters.NumberFilter(field_name="lightcurve", method='filter_nlc_lt', lookup_expr='lte')
-   
-   
-   def filter_name(self, queryset, name, value):
-      try:
-         data = Simbad.query_object(value)
-         ra = Angle(data['RA'][0], unit='hour').degree
-         dec = Angle(data['DEC'][0], unit='degree').degree
-         return queryset.filter(ra__range=[ra-15./3600., ra+15./3600.]).filter(dec__range=[dec-5./3600., dec+5./3600.])
-      except Exception:
-         return queryset.filter(name__icontains=value)
-   
-   def filter_coordinates(self, queryset, name, value):
-      ra, dec = value.split()
-      
-      if ':' in ra:
-         ra = Angle(ra, unit='hour').degree
-      else:
-         ra = Angle(ra, unit='degree').degree
-         
-      dec = Angle(dec, unit='degree').degree
-      
-      return queryset.filter(ra__range=[ra-15./3600., ra+15./3600.]).filter(dec__range=[dec-5./3600., dec+5./3600.])
+    '''
+    Filter definitions for table with stars
+    '''
+    #   Name filter
+    name = filters.CharFilter(
+        field_name="name",
+        method='filter_name',
+        lookup_expr='icontains',
+        )
+    #name = filters.CharFilter(
+        #field_name='name',
+        #method='filter_identifier',
+        #lookup_expr='icontains',
+        #)
 
-   
-   def filter_magnitude_gt(self, queryset, name, value):
-      return queryset.filter(photometry__band="GAIA2.G",  photometry__measurement__gte=value)
-   
-   def filter_magnitude_lt(self, queryset, name, value):
-      return queryset.filter(photometry__band="GAIA2.G",  photometry__measurement__lte=value)
-   
-   def filter_identifier(self, queryset, name, value):
-      return queryset.filter(identifier__name__icontains=value)
-   
-   def filter_nspec_gt(self, queryset, name, value):
-      return queryset.annotate(num_spec=Count('spectrum')).filter(num_spec__gte=value)
-   
-   def filter_nspec_lt(self, queryset, name, value):
-      return queryset.annotate(num_spec=Count('spectrum')).filter(num_spec__lte=value)
-   
-   def filter_nphot_gt(self, queryset, name, value):
-      return queryset.annotate(num_phot=Count('photometry')).filter(num_phot__gte=value)
-   
-   def filter_nphot_lt(self, queryset, name, value):
-      return queryset.annotate(num_phot=Count('photometry')).filter(num_phot__lte=value)
-   
-   def filter_nlc_gt(self, queryset, name, value):
-      return queryset.annotate(num_lc=Count('lightcurve')).filter(num_lc__gte=value)
-   
-   def filter_nlc_lt(self, queryset, name, value):
-      return queryset.annotate(num_lc=Count('lightcurve')).filter(num_lc__lte=value)
-   
-   class Meta:
-      model = Star
-      fields = ['project']
+    #   Coordinates filter
+    coordinates = filters.CharFilter(
+        field_name="ra",
+        method='filter_coordinates',
+        lookup_expr='icontains',
+        )
 
-   @property
-   def qs(self):
-      parent = super().qs
-      
-      parent = get_allowed_objects_to_view_for_user(parent, self.request.user)
-      
-      # get the column order from the GET dictionary
-      getter = self.request.query_params.get
-      if not getter('order[0][column]') is None:
-         order_column = int(getter('order[0][column]'))
-         order_name = getter('columns[%i][data]' % order_column)
-         if getter('order[0][dir]') == 'desc': order_name = '-'+order_name
-         
-         return parent.order_by(order_name)
-      else:
-         return parent
+    #   RA & DEC filter
+    ra = filters.RangeFilter(field_name="ra",)
+    dec = filters.RangeFilter(field_name="dec",)
+
+    #   Filter for G magnitudes
+    mag_min = filters.NumberFilter(
+        field_name="Gmag",
+        method='filter_magnitude_gt',
+        lookup_expr='gte',
+        )
+    mag_max = filters.NumberFilter(
+        field_name="Gmag",
+        method='filter_magnitude_lt',
+        lookup_expr='lte',
+        )
+
+    #   Classification filters
+    classification = filters.CharFilter(
+        field_name="classification",
+        lookup_expr='icontains',
+        )
+    classification_type = filters.MultipleChoiceFilter(
+        field_name="classification_type",
+        choices=Star.CLASSIFICATION_TYPE_CHOICES,
+        )
+
+    #   Status filter
+    status = filters.MultipleChoiceFilter(
+        field_name="observing_status",
+        choices=Star.OBSERVING_STATUS_CHOICES,
+        )
+
+    #   Tag filter
+    tags = filters.ModelMultipleChoiceFilter(queryset=Tag.objects.all())
+
+    #   Filter for # of photometry measurements, spectra, light curves
+    nphot_min = filters.NumberFilter(
+        field_name="photometry",
+        method='filter_obs_gt',
+        lookup_expr='gte',
+        )
+    nphot_max = filters.NumberFilter(
+        field_name="photometry",
+        method='filter_obs_lt',
+        lookup_expr='lte',
+        )
+
+    nspec_min = filters.NumberFilter(
+        field_name="spectrum",
+        method='filter_obs_gt',
+        lookup_expr='gte',
+        )
+    nspec_max = filters.NumberFilter(
+        field_name="spectrum",
+        method='filter_obs_lt',
+        lookup_expr='lte',
+        )
+
+    nlc_min = filters.NumberFilter(
+        field_name="lightcurve",
+        method='filter_obs_gt',
+        lookup_expr='gte',
+        )
+    nlc_max = filters.NumberFilter(
+        field_name="lightcurve",
+        method='filter_obs_lt',
+        lookup_expr='lte',
+        )
+
+
+    #   Method definitions for the filter definitions above
+    def filter_name(self, queryset, name, value):
+        try:
+            data = Simbad.query_object(value)
+            ra = Angle(data['RA'][0], unit='hour').degree
+            dec = Angle(data['DEC'][0], unit='degree').degree
+            return queryset.filter(ra__range=[ra-15./3600., ra+15./3600.]).\
+                filter(dec__range=[dec-5./3600., dec+5./3600.])
+        except Exception:
+            return queryset.filter(name__icontains=value)
+
+    def filter_coordinates(self, queryset, name, value):
+        ra, dec = value.split()
+
+        if ':' in ra:
+            ra = Angle(ra, unit='hour').degree
+        else:
+            ra = Angle(ra, unit='degree').degree
+
+        dec = Angle(dec, unit='degree').degree
+
+        return queryset.filter(ra__range=[ra-15./3600., ra+15./3600.]).filter(dec__range=[dec-5./3600., dec+5./3600.])
+
+
+    def filter_magnitude_gt(self, queryset, name, value):
+        return queryset.filter(
+            photometry__band="GAIA2.G",
+            photometry__measurement__gte=value,
+            )
+
+    def filter_magnitude_lt(self, queryset, name, value):
+        return queryset.filter(
+            photometry__band="GAIA2.G",
+            photometry__measurement__lte=value,
+            )
+
+    def filter_identifier(self, queryset, name, value):
+        return queryset.filter(identifier__name__icontains=value)
+
+    #   General method for the observations filter
+    #   - distinct=True is required to allow filter chains,
+    #     false results will be returned otherwise
+    def filter_obs_gt(self, queryset, name, value):
+        return queryset.annotate(num_obs=Count(name, distinct=True)).\
+            filter(num_obs__gte=value)
+
+    def filter_obs_lt(self, queryset, name, value):
+        return queryset.annotate(num_obs=Count(name, distinct=True)).\
+            filter(num_obs__lte=value)
+
+    class Meta:
+        model = Star
+        fields = ['project']
+
+    @property
+    def qs(self):
+        parent = super().qs
+
+        parent = get_allowed_objects_to_view_for_user(parent, self.request.user)
+
+        #   Get the column order from the GET dictionary
+        getter = self.request.query_params.get
+        if not getter('order[0][column]') is None:
+            order_column = int(getter('order[0][column]'))
+            order_name = getter('columns[%i][data]' % order_column)
+            if getter('order[0][dir]') == 'desc': order_name = '-'+order_name
+
+            return parent.order_by(order_name)
+        else:
+            return parent
 
 
 class StarViewSet(viewsets.ModelViewSet):
@@ -166,56 +226,56 @@ class StarViewSet(viewsets.ModelViewSet):
    list:
    Returns a list of all stars/objects in the database
    """
-   
+
    queryset = Star.objects.all()
    serializer_class = StarSerializer
-   
+
    filter_backends = (DjangoFilterBackend,)
    filterset_class = StarFilter
-   
+
    def get_serializer_class(self):
       if self.action == 'list':
          return StarListSerializer
       if self.action == 'retrieve':
          return StarSerializer
       return StarSerializer
-   
-   
+
+
 #class StarViewSet(viewsets.ModelViewSet):
    #queryset = Star.objects.all()
    #serializer_class = StarListSerializer
-   
+
    ##def list(self, request):
         ##queryset = User.objects.all()
         ##serializer = UserSerializer(queryset, many=True)
         ##return Response(serializer.data)
-   
+
    #def retrieve(self, request, pk=None):
       #star = Star.objects.get(pk=pk)
       #serializer = StarSerializer(star)
       #return Response(serializer.data)
-   
+
    #def update(self, request, pk=None):
       #star = Star.objects.get(pk=pk)
       #serializer = StarSerializer(star)
       #return Response(serializer.data)
-   
+
    #def partial_update(self, request, pk):
       #star = Star.objects.get(pk=pk)
       #serializer = StarSerializer(star, data=request.data, partial=True) # set partial=True to update a data partially
-      
+
       #print request.data
-      
+
       ##if 'tag_set' in request.data:
          ##star.tag_set = request.data['tag_set']
          ##star.save()
          ##print 'new Tags'
-      
+
       #if serializer.is_valid():
          #print serializer.validated_data
          #serializer.save()
          #return JsonResponse(status=201, data=serializer.data)
-         
+
       #return JsonResponse(status=400, data="wrong parameters")
 
 
@@ -224,22 +284,22 @@ class StarViewSet(viewsets.ModelViewSet):
 # ===============================================================
 
 class TagFilter(filters.FilterSet):
-   
+
    class Meta:
       model = Tag
       fields = ['project',]
-      
+
    @property
    def qs(self):
       parent = super().qs
-      
+
       return get_allowed_objects_to_view_for_user(parent, self.request.user)
-      
+
 
 class TagViewSet(viewsets.ModelViewSet):
    queryset = Tag.objects.all()
    serializer_class = TagSerializer
-   
+
    filter_backends = (DjangoFilterBackend,)
    filterset_class = TagFilter
 
@@ -251,11 +311,11 @@ class TagViewSet(viewsets.ModelViewSet):
 # identifiers doesn't have a special filter, but still only returns the identifiers from allowed projects
 # this does require to define a custom get_queryset, which also requires the addition of a basename in the
 # router in urls.py
-   
+
 class IdentifierViewSet(viewsets.ModelViewSet):
    #queryset = Identifier.objects.all()
    serializer_class = IdentifierListSerializer
-   
+
    def list(self, request):
       queryset = Identifier.objects.all()
       star = request.query_params.get('star', None)
@@ -263,7 +323,7 @@ class IdentifierViewSet(viewsets.ModelViewSet):
          queryset = queryset.filter(star=star)
       serializer = IdentifierListSerializer(queryset, many=True)
       return Response(serializer.data)
-      
+
    def get_queryset(self):
       qs = Identifier.objects.all()
       return get_allowed_objects_to_view_for_user(qs, self.request.user)

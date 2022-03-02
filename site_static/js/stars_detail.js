ba168d36c9971883f11385a95f807f985c001874
Fabian Mattig <88104880+Fabmat1@users.noreply.github.com>
2022-03-02 01:02:40 +0100

    Added ability to add Photometry when none is present

--- site_static/js/stars_detail.js
+++ site_static/js/stars_detail.js
@@ -1,243 +1,267 @@
-
 var star = {}
 let photeditenabled = false
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-   // load the star info
-   var star_id = $('#tag_list').attr('star_id')
-   $.ajax({
-      url : "/api/systems/stars/"+star_id+'/',
-      type : "GET",
-      success : function(json) {
-         star = json;
-         makepage();
-      },
-      error : function(xhr,errmsg,err) {
+    // load the star info
+    var star_id = $('#tag_list').attr('star_id')
+    $.ajax({
+        url: "/api/systems/stars/" + star_id + '/',
+        type: "GET",
+        success: function (json) {
+            star = json;
+            makepage();
+        },
+        error: function (xhr, errmsg, err) {
             console.log(xhr.status + ": " + xhr.responseText);
-      }
-   });
-
-   // Initializing all dialog windows
-   var update_note_window = $("#noteEdit").dialog({autoOpen: false,
-            width: 'auto',
-            modal: true});
-
-   var add_identifier_window = $("#identifierAdd").dialog({autoOpen: false,
-            width: 'auto',
-            modal: true});
-
-   var update_tag_window = $("#tadAdd").dialog({autoOpen: false,
-            width: 'auto',
-            modal: true});
-   var all_parameters_window = $("#allParameters").dialog({autoOpen: false,
-            title: 'Parameter Overview',
-            width: 'auto',
-            height: 'auto',
-            modal: true});
-
-   // add event listeners
-   $( "#noteEditButton").click( openNoteUpdateBox );
-   $( "#identifierAddButton").click( openIdentifierAddBox );
-   $( "#tagEditButton").click( openTagEditBox );
-   $( "#allParameterButton").click( openAllParameterBox );
-   $( "#photedit").click( enablephotedit );
-
-   // Delete identifier on click
-   $(".identifier").on('click', 'i[id^=delete-identifier-]', function(){
-      var identifier_pk = $(this).attr('id').split('-')[2];
-      delete_identifier(identifier_pk);
-   });
-
-   // toggle the observation section
-   $('#toggle-obs').click( function (){
-      $('#obs_summary').slideToggle();
-      $('#obs_detail').slideToggle();
-
-      if ($(this).text() == 'visibility') {
-         $(this).text('visibility_off')
-      } else {
-         $(this).text('visibility')
-      }
-
-      $(".photvalconst").each(function(){
-          let band = $(this).data("band")
-          $(".dropdownlink").each(function (){
-              if( band === $(this).data("band")){
-                $(this).hide()
-            }
-          })
-      });
-        $(".dropdownlink").click(function(e) {
-            $(this).hide()
-            let band = $(this).data("band")
-            $(".phottablerow").each(function (i, row){
-                let rowband = $(row).find('td[class="mono"]').text()
-                if(rowband === band){
-                    $(this).show()
-            }
-            })
-        })
+        }
     });
 
+    // Initializing all dialog windows
+    var update_note_window = $("#noteEdit").dialog({
+        autoOpen: false,
+        width: 'auto',
+        modal: true
+    });
+
+    var add_identifier_window = $("#identifierAdd").dialog({
+        autoOpen: false,
+        width: 'auto',
+        modal: true
+    });
+
+    var update_tag_window = $("#tadAdd").dialog({
+        autoOpen: false,
+        width: 'auto',
+        modal: true
+    });
+    var all_parameters_window = $("#allParameters").dialog({
+        autoOpen: false,
+        title: 'Parameter Overview',
+        width: 'auto',
+        height: 'auto',
+        modal: true
+    });
+
+    // add event listeners
+    $("#noteEditButton").click(openNoteUpdateBox);
+    $("#identifierAddButton").click(openIdentifierAddBox);
+    $("#tagEditButton").click(openTagEditBox);
+    $("#allParameterButton").click(openAllParameterBox);
+    $("#photedit").click(enablephotedit);
+
+    // Delete identifier on click
+    $(".identifier").on('click', 'i[id^=delete-identifier-]', function () {
+        var identifier_pk = $(this).attr('id').split('-')[2];
+        delete_identifier(identifier_pk);
+    });
+
+    // toggle the observation section
+    $('#toggle-obs').click(function () {
+        $('#obs_summary').slideToggle();
+        $('#obs_detail').slideToggle();
+
+        if ($(this).text() == 'visibility') {
+            $(this).text('visibility_off')
+        } else {
+            $(this).text('visibility')
+        }
+
+        $(".photvalconst").each(function () {
+            let band = $(this).data("band")
+            $(".dropdownlink").each(function () {
+                if (band === $(this).data("band")) {
+                    $(this).hide()
+                }
+            })
+        });
+
+
+    });
+    $(".dropdownlink").click(function (e) {
+        $(this).hide()
+        let band = $(this).data("band")
+        $(".phottablerow").each(function (i, row) {
+            let rowband = $(row).find('td[class="mono"]').text()
+            if (rowband === band) {
+                $(this).show()
+            }
+        })
+        let novaluesrow = $("#tr-novaluesyet")
+        if (novaluesrow.is(":visible")){
+            novaluesrow.hide()
+        }
+    })
 });
 
 // Create the page dynamicaly
-function makepage () {
-   show_tags () // handle the tags
-};
+function makepage() {
+    show_tags() // handle the tags
+}
 
-function show_tags () {
-   // Check which tags should be displayed in the tag window, and check the
-   // included tags in the tag edit window.
-   $('#tag_list').empty(); // remove all existing tags
-   for (i=0; i<star.tags.length; i++) {
-      var tag = star.tags[i];
-      $('#tag_pk_'+tag.pk).prop("checked", true);
-      $('#tag_list').append(
-      "<div class='tag' id='tag-"+tag.pk+"' style='border-color:"+tag.color+"' title='"+tag.info+"'>"+tag.name+"</div>"
-      );
-   };
+function show_tags() {
+    // Check which tags should be displayed in the tag window, and check the
+    // included tags in the tag edit window.
+    $('#tag_list').empty(); // remove all existing tags
+    for (i = 0; i < star.tags.length; i++) {
+        var tag = star.tags[i];
+        $('#tag_pk_' + tag.pk).prop("checked", true);
+        $('#tag_list').append(
+            "<div class='tag' id='tag-" + tag.pk + "' style='border-color:" + tag.color + "' title='" + tag.info + "'>" + tag.name + "</div>"
+        );
+    }
+
 }
 
 
 // Functionality to open the edit/update windows
 function openNoteUpdateBox() {
-   update_note_window = $("#noteEdit").dialog({
-      buttons: { "Update": updateNote },
-      close: function() { update_note_window.dialog( "close" ); }
-   });
+    update_note_window = $("#noteEdit").dialog({
+        buttons: {"Update": updateNote},
+        close: function () {
+            update_note_window.dialog("close");
+        }
+    });
 
-   update_note_window.dialog( "open" );
-   $("#edit-message").val( $("#noteField").text().trim() );
-};
+    update_note_window.dialog("open");
+    $("#edit-message").val($("#noteField").text().trim());
+}
 
 function openIdentifierAddBox() {
-   add_identifier_window = $("#identifierAdd").dialog({
-      buttons: { "Add": create_identifier },
-      close: function() { add_identifier_window.dialog( "close" ); }
-   });
+    add_identifier_window = $("#identifierAdd").dialog({
+        buttons: {"Add": create_identifier},
+        close: function () {
+            add_identifier_window.dialog("close");
+        }
+    });
 
-   add_identifier_window.dialog( "open" );
-};
+    add_identifier_window.dialog("open");
+}
 
 function openTagEditBox() {
-   update_tag_window = $("#tadAdd").dialog({
-      buttons: { "Update": update_tags },
-      close: function() { update_tag_window.dialog( "close" ); }
-   });
+    update_tag_window = $("#tadAdd").dialog({
+        buttons: {"Update": update_tags},
+        close: function () {
+            update_tag_window.dialog("close");
+        }
+    });
 
-   update_tag_window.dialog( "open" );
-};
+    update_tag_window.dialog("open");
+}
 
 function openAllParameterBox() {
-   all_parameters_window = $("#allParameters").dialog({
-      close: function() { all_parameters_window.dialog( "close" ); }
-   });
+    all_parameters_window = $("#allParameters").dialog({
+        close: function () {
+            all_parameters_window.dialog("close");
+        }
+    });
 
-   all_parameters_window.dialog( "open" );
-};
+    all_parameters_window.dialog("open");
+}
 
 // Update the comment of the star
 function updateNote() {
-   var star_id = $('#noteEditButton').attr('star_id')
-   $.ajax({
-      url : "/api/systems/stars/"+star_id+'/',
-      type : "PATCH",
-      data : { note: $("#edit-message").val().trim() },
+    var star_id = $('#noteEditButton').attr('star_id')
+    $.ajax({
+        url: "/api/systems/stars/" + star_id + '/',
+        type: "PATCH",
+        data: {note: $("#edit-message").val().trim()},
 
-      success : function(json) {
-            update_note_window.dialog( "close" );
+        success: function (json) {
+            update_note_window.dialog("close");
             $("#noteField").text(json.note);
             star.note = json.note;
-      },
+        },
 
-      error : function(xhr,errmsg,err) {
+        error: function (xhr, errmsg, err) {
             console.log(xhr.status + ": " + xhr.responseText);
-      }
-   });
+        }
+    });
 }
 
 // Create an identifier
-function create_identifier(){
-   $.ajax({
-      url : "/api/systems/identifiers/",
-      type : "POST",
-      data : { star : $('#identifierAddButton').attr('star_id'),
-               name : $('#identifierAddtext').val() ,
-               href : $('#identifierAddhref').val(),
-      },
+function create_identifier() {
+    $.ajax({
+        url: "/api/systems/identifiers/",
+        type: "POST",
+        data: {
+            star: $('#identifierAddButton').attr('star_id'),
+            name: $('#identifierAddtext').val(),
+            href: $('#identifierAddhref').val(),
+        },
 
-      success : function(json) {
+        success: function (json) {
 //          add identifier to the list and close the window on success
-            add_identifier_window.dialog( "close" );
-            if (json.href != ""){
-               $("#identifier_list").prepend("<div class=\"identifier\" id=\"identifier-"+json.pk+"\"> <a href=\"" +
-               json.href+"\" target=\"_blank\">"+json.name+
-               "</a> <i class=\"material-icons button delete\" id=\"delete-identifier-"+
-               json.pk+"\">delete</i></div>");
+            add_identifier_window.dialog("close");
+            if (json.href != "") {
+                $("#identifier_list").prepend("<div class=\"identifier\" id=\"identifier-" + json.pk + "\"> <a href=\"" +
+                    json.href + "\" target=\"_blank\">" + json.name +
+                    "</a> <i class=\"material-icons button delete\" id=\"delete-identifier-" +
+                    json.pk + "\">delete</i></div>");
             } else {
-               $("#identifier_list").prepend("<div class=\"identifier\" id=\"identifier-"+json.pk+"\">"+json.name+
-               "<i class=\"material-icons button delete\" id=\"delete-identifier-"+
-               json.pk+"\">delete</i></div>");
+                $("#identifier_list").prepend("<div class=\"identifier\" id=\"identifier-" + json.pk + "\">" + json.name +
+                    "<i class=\"material-icons button delete\" id=\"delete-identifier-" +
+                    json.pk + "\">delete</i></div>");
             }
-      },
+        },
 
-      error : function(xhr,errmsg,err) {
+        error: function (xhr, errmsg, err) {
             console.log(xhr.status + ": " + xhr.responseText);
-      }
-   });
-};
+        }
+    });
+}
 
 // Delete an identifier
-function delete_identifier(identifier_pk){
-    if (confirm('Are you sure you want to remove this Identifier?')==true){
+function delete_identifier(identifier_pk) {
+    if (confirm('Are you sure you want to remove this Identifier?') == true) {
         $.ajax({
-            url : "/api/systems/identifiers/"+identifier_pk+'/',
-            type : "DELETE",
+            url: "/api/systems/identifiers/" + identifier_pk + '/',
+            type: "DELETE",
 
-            success : function(json) {
-              $('#identifier-'+identifier_pk).hide();
+            success: function (json) {
+                $('#identifier-' + identifier_pk).hide();
             },
 
-            error : function(xhr,errmsg,err) {
+            error: function (xhr, errmsg, err) {
                 console.log(xhr.status + ": " + xhr.responseText);
             }
         });
     } else {
         return false;
     }
-};
+}
 
 // Update the tags attached to this star
-function update_tags(){
-   var new_tags = $("#tagOptions input:checked").map(
-       function () { return this.value; } ).get();
-   var star_pk = $('#tagEditButton').attr('star_id')
-   $.ajax({
-      url : "/api/systems/stars/"+star_pk+'/',
-      type : "PATCH",
-      contentType: "application/json; charset=utf-8",
+function update_tags() {
+    var new_tags = $("#tagOptions input:checked").map(
+        function () {
+            return this.value;
+        }).get();
+    var star_pk = $('#tagEditButton').attr('star_id')
+    $.ajax({
+        url: "/api/systems/stars/" + star_pk + '/',
+        type: "PATCH",
+        contentType: "application/json; charset=utf-8",
 
-      data : JSON.stringify({ "tag_ids": new_tags }),
+        data: JSON.stringify({"tag_ids": new_tags}),
 
-      success : function(json) {
-         // update the tags of the star variable, and update the page
-         star.tags = json.tags;
-         show_tags();
-         update_tag_window.dialog( "close" );
-      },
+        success: function (json) {
+            // update the tags of the star variable, and update the page
+            star.tags = json.tags;
+            show_tags();
+            update_tag_window.dialog("close");
+        },
 
-      error : function(xhr,errmsg,err) {
-         console.log(xhr.status + ": " + xhr.responseText);
-      }
-   });
+        error: function (xhr, errmsg, err) {
+            console.log(xhr.status + ": " + xhr.responseText);
+        }
+    });
 }
 
 // Enable Editing Photometry on Button click
-function enablephotedit(){
+function enablephotedit() {
     photedit = $("#photedit");
-    if(photeditenabled){
+    if (photeditenabled) {
         photedit.show();
         $("#phot-submit-btn").hide();
         $(".photval").each(function (ind, obj) {
@@ -247,8 +271,7 @@ function enablephotedit(){
         $("#submitrow").hide();
         $("#addband").hide();
         photeditenabled = false;
-        }
-    else{
+    } else {
         photedit.hide();
         $("#phot-submit-btn").show();
         let existingbands = [];
@@ -268,7 +291,7 @@ function enablephotedit(){
         $(".photvalinp").each(function (ind, obj) {
             let band = $(this).data("band");
             let arrind = jQuery.inArray(band, existingbands);
-            if (arrind > -1){
+            if (arrind > -1) {
                 $(this).closest('tr').show();
                 $(this).find('input[type=number]').val(bandvalues[arrind]);
                 $(this).parent().find('td[class=photerrinp]').find('input[type=number]').val(banderrvalues[arrind]);
@@ -281,19 +304,18 @@ function enablephotedit(){
 }
 
 
-function Toggledropdown(){
+function Toggledropdown() {
     $("#addbanddropdown").toggleClass("show");
 }
 
 
-
 // Close the dropdown menu if the user clicks outside of it
-$(window).click(function(e) {
-  if (!e.target.matches('.dropbtn')) {
-    $(".dropdown-content").each(function (i){
-      if ($(this).hasClass('show')) {
-        $(this).removeClass('show');
-      }
-    })
-  }
+$(window).click(function (e) {
+    if (!e.target.matches('.dropbtn')) {
+        $(".dropdown-content").each(function (i) {
+            if ($(this).hasClass('show')) {
+                $(this).removeClass('show');
+            }
+        })
+    }
 })
